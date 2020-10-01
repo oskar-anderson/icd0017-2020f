e87402813ddfd8e4ee8d4af777e859515533b631
kaande <kaande@ttu.ee>
2020-10-01 23:13:53 +0300

    colored circles

--- Connect4/app/src/androidTest/java/com/example/connect4/ExampleInstrumentedTest.kt
+++ Connect4/app/src/androidTest/java/com/example/connect4/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tictactoe", appContext.packageName)
+        assertEquals("com.example.connect4", appContext.packageName)
     }
 }
--- Connect4/app/src/main/java/com/example/connect4/MainActivity.kt
+++ Connect4/app/src/main/java/com/example/connect4/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.connect4
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    var tileWidth = 48;
-    var tileHeight = 48;
 
     val buttons = arrayOf<Array<Button>>()
     var nextMoveByX = true;
@@ -21,10 +22,37 @@ class MainActivity : AppCompatActivity() {
         //playerTwoScore = (TextView) findViewById(R.id.playerTwoScore)
     }
 
-    fun gameButtonOnClick(view: View){
-        if ((view as Button).text == ""){
+    fun gameColumnOnClick(view: View){
+        if (true){
             (view as Button).text = if(nextMoveByX) "X" else "0"
+            (view as Button).background = Drawable.createFromPath("#F00")
             nextMoveByX = !nextMoveByX
+        }
+    }
+
+    fun gameButtonOnClick(view: View){
+        if (view.tag != "used"){
+            view.setTag("used");
+            (view as Button).text = if(nextMoveByX) "X" else "0"
+            if (nextMoveByX){
+                view.setBackgroundResource(R.drawable.circle_red)
+                findViewById<TextView>(R.id.currentPlayerValue).text = "Yellow"
+            }
+            else {
+                view.setBackgroundResource(R.drawable.circle_yellow)
+                findViewById<TextView>(R.id.currentPlayerValue).text = "Red"
+            }
+            val roundNumber = findViewById<TextView>(R.id.roundNumber)
+            roundNumber.text = Integer.toString(Integer.parseInt(roundNumber.text as String) + 1)
+            nextMoveByX = !nextMoveByX
+
+
+            //var red = Drawable.createFromPath("#F00");
+            //var yellow = Drawable.createFromPath("#FF0");
+
+            //view.background = resources.getDrawable(R.drawable.circle_red)
+            //view.setCompoundDrawablesWithIntrinsicBounds(R.drawable.circle_red, 0, R.drawable.circle_red, 0)
+            //(view as Button).setBackgroundColor(Color.RED);
         }
     }
 }
